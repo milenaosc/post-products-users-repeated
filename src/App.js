40c32073b30c76a26e016733dee88f214af92a1f
milenaosc <milenadigitalsosc@gmail.com>
2023-05-14 10:55:51 +0200

    added NavBar with initial styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./_index.scss";
+import "./index.scss";
 import React from "react";
 import { Outlet } from "react-router";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Outlet />
     </div>
   );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,13 +4,13 @@ import "./_navbar.scss";
 
 const NavBar = () => {
   return (
-    <>
+    <div className="navbar container">
       <div>Logo</div>
       <ul>
         <NavLink path="/">Home</NavLink>
         <NavLink path="/posts">Posts</NavLink>
       </ul>
-    </>
+    </div>
   );
 };
 
